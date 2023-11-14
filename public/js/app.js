f17ad7adf9e5cec2f4af5da0475c8735f86be5b9
Fabio Adani <fabio.adani@ynap.com>
2023-11-14 18:00:49 +0100

    Implement login and status handling, but still getting 401 access denied

--- public/js/app.js
+++ public/js/app.js
@@ -14,4 +14,41 @@ const configureClient = async () => {
 
 window.onload = async () => {
     await configureClient();
+
+    updateUI();
+    
+    const isAuthenticated = await auth0Client.isAuthenticated();
+
+    if (isAuthenticated) {
+      // show the gated content
+      return;
+    }
+  
+    // NEW - check for the code and state parameters
+    const query = window.location.search;
+    if (query.includes("code=") && query.includes("state=")) {
+  
+      // Process the login state
+      await auth0Client.handleRedirectCallback();
+      
+      updateUI();
+  
+      // Use replaceState to redirect the user away and remove the querystring parameters
+      window.history.replaceState({}, document.title, "/");
+    }
 }
+
+const updateUI = async () => {
+    const isAuthenticated = await auth0Client.isAuthenticated();
+  
+    document.getElementById("btn-logout").disabled = !isAuthenticated;
+    document.getElementById("btn-login").disabled = isAuthenticated;
+};
+
+const login = async () => {
+    await auth0Client.loginWithRedirect({
+        authorizationParams: {
+            redirect_uri: window.location.origin
+        }
+    });
+};
