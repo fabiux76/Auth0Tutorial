a80274985577fb913ad3ddb0be533cfb67b6c6f1
Fabio Adani <fabio.adani@ynap.com>
2023-11-15 16:51:32 +0100

    correct order of mappings

--- server.js
+++ server.js
@@ -1,40 +1,44 @@
 const express = require("express");
-const { join } = require("path");
 const { auth } = require("express-oauth2-jwt-bearer");
+const { join } = require("path");
 const authConfig = require("./auth_config.json");
 
 const app = express();
 
-// Serve static assets from the /public folder
+// Serve assets from the /public folder
 app.use(express.static(join(__dirname, "public")));
 
+// Create the JWT validation middleware
 const checkJwt = auth({
   audience: authConfig.audience,
   issuerBaseURL: `https://${authConfig.domain}`
 });
 
+// Create an endpoint that uses the above middleware to
+// protect this route from unauthorized requests
+app.get("/api/external", checkJwt, (req, res) => {
+  res.send({
+    msg: "Your access token was successfully validated!"
+  });
+});
+
+// Serve the auth configuration file
+app.get("/auth_config.json", (req, res) => {
+  res.sendFile(join(__dirname, "auth_config.json"));
+});
+
+// Serve the index page to everything else
+app.get("/*", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+// Error handler
 app.use(function(err, req, res, next) {
   if (err.name === "UnauthorizedError") {
     return res.status(401).send({ msg: "Invalid token" });
   }
 
   next(err, req, res);
-});
-
-// Endpoint to serve the configuration file
-app.get("/auth_config.json", (req, res) => {
-  res.sendFile(join(__dirname, "auth_config.json"));
-});
-
-// Serve the index page for all other requests
-app.get("/*", (_, res) => {
-  res.sendFile(join(__dirname, "index.html"));
-});
-
-app.get("/api/external", checkJwt, (req, res) => {
-  res.send({
-    msg: "Your access token was successfully validated!"
-  });
 });
 
 // Listen on port 3000
